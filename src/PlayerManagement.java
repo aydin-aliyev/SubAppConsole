bf9b7d52f16f1bc26d6f52aa979da9e17d5fe9be
Aydin Aliyev <aydin.al1yev.0743x>
2025-05-20 00:54:53 +0400

    added a loop for creating players

--- src/PlayerManagement.java
+++ src/PlayerManagement.java
@@ -1,3 +1,4 @@
+import java.sql.Connection;
 import java.util.*;
 
 public class PlayerManagement extends InHouse{
@@ -17,27 +18,44 @@ public class PlayerManagement extends InHouse{
     }
 
     void addPlayer() {
+
+        while (true){
         System.out.println("add player:");
 
-        System.out.println("Name: ");
+        System.out.print("Name: ");
         String name = scanner.nextLine();
 
-        System.out.println("Last Name: ");
+        System.out.print("Last Name: ");
         String lastname = scanner.nextLine();
 
-        System.out.println("Belt: ");
+        System.out.print("Belt: ");
         String belt = scanner.nextLine();
 
-        System.out.println("Age: ");
+        System.out.print("Age: ");
         int age = scanner.nextInt();
+        scanner.nextLine(); // очищение буфера после nextInt
 
 
         playerList.add(new Player(name, lastname, age, belt));
         System.out.println("Player was added");
-        System.out.println("Heey");
 
 
+        System.out.print("Do you want to add player (y/N)? ");
+        String answer  =  scanner.nextLine();
+        System.out.println("-----------------------------------");
+
+
+
+        switch (answer){
+            case "y":
+                break;
+            case "N":
+                System.out.println("Exiting");
+                return;
+            default:
+                System.out.println("Wrong");
+        }
+
+        }
     }
-
-
 }
